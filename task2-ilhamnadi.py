dcd6e7302057ce51802ed46617041e619c01ed2c
ilhamsaputranadi <147223769+ilhamsaputranadi@users.noreply.github.com>
2023-10-11 12:38:57 +0700

    Add files via upload

---
+++ task2-ilhamnadi.py
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width   , initial-scale=1.0" />
+    <title>PRAKTIKUM 1</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+  </head>
+  <body>
+    <h1 style="color: green">SELAMAT DATANG DI PRATIKUM 1</h1>
+    <hr />
+    <h2 style="color: green">MY PROFILE</h3>
+    <ol>
+      <li>Nama: Muhammad ilham nadi saputra</li>
+      <li>Email:ilhamsaputra200418@gmail.com</li>
+    </ol>
+    <h1 style="color: green">DEKRIPSI KOTA TERINDAH</h1>
+    <small
+      >Ini adalah halaman web kota terindah yang ada di indonesia.kota ini
+      menurut saya kota terindah yang ada di indonesia yang wajib kalian
+      kunjungi yang memiliki keindahan alam,sejarah maupun budaya yang
+      khas.</small
+    >
+    <h2 style="color: green">FOTO KOTA FAVORIT</h2>
+    <div class="img" style="display: flex;">
+        <img
+        src="https://awsimages.detik.net.id/community/media/visual/2023/01/10/rencana-jalan-berbayar-di-jakarta_169.jpeg?w=1200"
+        width="300px"
+        alt=""
+      /><br />
+      <img
+        src="https://awsimages.detik.net.id/community/media/visual/2022/03/14/warga-kunjungi-alun-alun-kota-bogor-1_169.jpeg?w=1200"
+        width="300px"
+        alt=""
+      /><br />
+      <img
+        src="https://asset.kompas.com/crops/0DzHZaA8t7AYz0lY5aNkLIi_KIQ=/17x0:1000x655/750x500/data/photo/2023/02/19/63f1ded487f25.jpg"
+        width="300px"
+        alt=""
+      />
+    </div>
+   <br />
+
+    <h2 style="color: green">DAFTAR KOTA FAVORIT</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>NAMA KOTA</th>
+          <th>ICON KOTA</th>
+          <th>JUMLAH PENDUDUK</th>
+          <th>KEUNIKAN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Jakarta</td>
+          <td>Monas</td>
+          <td>11,24 juta jiwa</td>
+          <td>kota pemerintahan pusat</td>
+        </tr>
+        <tr>
+          <td>Bogor</td>
+          <td>Tugu Kujang</td>
+          <td>1.063.513 jiwa</td>
+          <td>Kebun raya bogor</td>
+        </tr>
+        <tr>
+          <td>Ciamis</td>
+          <td>Kota Manis</td>
+          <td>103.278.310 jiwa,</td>
+          <td>Jembatan cirahong</td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
